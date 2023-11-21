7be5a85a9542cdd9b4c65c953030e5fa1d2733b1
MrBochh <jorgealberto1436@gmail.com>
2023-11-21 10:55:46 -0600

    tco myReverse

--- 5.hs
+++ 5.hs
@@ -12,3 +12,16 @@ myReverseTCO (x:xs) c = myReverseTCO (init x:xs) (c : (last xs))
 myReverse' xs = myReverseTCO xs 0
 -}
 
+tcoRev [] c = c 
+tcoRev xs c = tcoRev (init xs) (c ++ [last xs])
+
+myReverse' xs = tcoRev xs []
+
+{-
+reverse in Prelude is defined like this 
+
+reverse          :: [a] -> [a]
+reverse          =  foldl (flip (:)) []
+
+I dont understand that a foldl and flip means 
+-}
